c8f9a9ddcf23abcf3da861be4e01a326be6806ba
abdellatif rhounan <abdellatif.rhounan@gmail.com>
2023-05-07 01:25:56 +0000

    Home Section's layout handled for large screen

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
-// ========= Scroll Active Links =======
 let sections = document.querySelectorAll('section');
 let header = document.querySelector("header");
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-  let top = window.scrollY;
+  let scrollPos = window.scrollY;
 
+  //========== sticky navbar ==========
+  header.classList.toggle('sticky', scrollPos > 50);
+
+  // ========= Scroll Active Links =======
   sections.forEach(sec => {
-    let offset = sec.offsetTop - 150;
+    let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if (top >= offset && top < (offset + height)) {
-      navLinks.forEach(links => {
-        links.classList.remove("active");
-        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+    if (scrollPos >= offset && scrollPos < (offset + height)) {
+      navLinks.forEach(link => {
+        link.classList.remove("active");
       });
+      document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
     }
   });
-
-  //========== sticky navbar ==========
-  header.classList.toggle('sticky', top > 100);
 };
