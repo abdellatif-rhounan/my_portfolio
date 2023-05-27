ab9fd18a6b49c6ada9c88a8b40d6f18c2307e38e
abdellatif rhounan <abdellatif.rhounan@gmail.com>
2023-05-27 22:28:37 +0000

    Scroll javascript code modified for best performance

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,40 @@
-let sections = document.querySelectorAll('section');
 let header = document.querySelector("header");
 let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let secData = [];
+
+// ======= Data of All Sections ======
+sections.forEach(sec => {
+  let arr = [];
+  let id = sec.getAttribute('id');
+  let offset = sec.offsetTop - 50;
+  let height = sec.offsetHeight;
+  arr.push(id, offset, offset + height);
+  secData.push(arr);
+});
+
+// ======= initial sticky class check ======
+window.onload = () => {
+  header.classList.toggle('sticky', window.scrollY > 50);
+};
 
 window.onscroll = () => {
   let scrollPos = window.scrollY;
 
-  //========== sticky navbar ==========
-  header.classList.toggle('sticky', scrollPos > 50);
-
-  // ========= Scroll Active Links =======
-  sections.forEach(sec => {
-    let offset = sec.offsetTop - 50;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if (scrollPos >= offset && scrollPos < (offset + height)) {
+  // ========= Scroll Active Link =======
+  let i = 0;
+  secData.forEach(sec => {
+    if (scrollPos >= sec[1] && scrollPos < (sec[2])) {
       navLinks.forEach(link => {
         link.classList.remove("active");
       });
-      document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      navLinks[i].classList.add("active");
+
+      //========== sticky navbar ==========
+      if (sec[0] === 'home') {
+        header.classList.toggle('sticky', scrollPos > 50);
+      }
     }
+    i++;
   });
 };
