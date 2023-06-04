36fa626be4417c84482a16dc67955344d062764d
abdellatif rhounan <abdellatif.rhounan@gmail.com>
2023-06-04 15:33:48 +0000

    Project Maintained | removing other breakpoints | js code maintained

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,28 @@
-let header = document.querySelector("header");
+let header = document.getElementById("header");
 let navLinks = document.querySelectorAll('header nav a');
 let sections = document.querySelectorAll('section');
 let secData = [];
-
-// ======= Data of All Sections ======
-sections.forEach(sec => {
-  let arr = [];
-  let id = sec.getAttribute('id');
-  let offset = sec.offsetTop - 50;
-  let height = sec.offsetHeight;
-  arr.push(id, offset, offset + height);
-  secData.push(arr);
-});
 
 // ======= initial sticky class check ======
 window.onload = () => {
   header.classList.toggle('sticky', window.scrollY > 50);
 };
 
+// ======= Data of All Sections ======
+sections.forEach(sec => {
+  let arr = [];
+  let id = sec.getAttribute('id');
+  let startPos = sec.offsetTop - 50;
+  let endPos = startPos + sec.offsetHeight;
+  arr.push(id, startPos, endPos);
+  secData.push(arr);
+});
+
+// ======= Scroll Event ========
 window.onscroll = () => {
   let scrollPos = window.scrollY;
 
-  // ========= Scroll Active Link =======
+  // ========= Header Active Link =======
   let i = 0;
   secData.forEach(sec => {
     if (scrollPos >= sec[1] && scrollPos < (sec[2])) {
@@ -31,9 +32,7 @@ window.onscroll = () => {
       navLinks[i].classList.add("active");
 
       //========== sticky navbar ==========
-      if (sec[0] === 'home') {
-        header.classList.toggle('sticky', scrollPos > 50);
-      }
+      header.classList.toggle('sticky', scrollPos > 50);
     }
     i++;
   });
